7f8ddcf5c7b0175510e34debc6b8797b8591cf4c
Abhik Shil <50538312+AbhikShil@users.noreply.github.com>
2024-05-09 22:12:34 +0530

    Add files via upload

---
+++ Placement Materials/js/question1.js
@@ -0,0 +1,379 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "If a car runs at 30kmph, it reaches its destination late by 17 minutes but if it runs at 50kmph , it is late by 8 minutes only . The correct time for the car to complete its journey is:?",
+    answer: "5.5 minutes",
+    options: [
+      "5.5 minutes",
+      "5 minutes",
+      "10 minues",
+      "None"
+    ]
+  },
+    {
+    numb: 2,
+    question: "A lady is facing South-East. She turns 180 degrees in the clockwise direction, then 360 degrees in the anticlockwise direction and then another 270 degrees in the clockwise direction. Which direction is she facing now?",
+    answer: "South",
+    options: [
+      "South",
+      "South-West",
+      "West",
+      "South-East"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Sai Ram travels from Delhi to Rajasthan at a speed of 50 kmph and returns by increasing his speed by 50%. What is the average speed in his entire journey?",
+    answer: "948.6",
+    options: [
+      "655.5",
+      "885.5",
+      "544",
+      "948.6"
+    ]
+  },
+  {
+    numb:4 ,
+    question: "Select the word or phrase which best expresses the meaning of the given word.SUFFOCATE (OPPOSITE)",
+    answer: "Release",
+    options: [
+      "Curb",
+      "Restrain",
+      "Release",
+      "Stie"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Rocky decided to cover a distance of 12 km in 80 minutes. He decided to cover two-thirds of the distance at 8 km/hr and the remaining at some different speed. Find the speed after covering two-thirds of the distance?",
+    answer: "12 kmph",
+    options: [
+      "20 kmph",
+      "8 kmph",
+      "12 kmph",
+      "10 kmph"
+    ]
+  },
+  {
+    numb: 6,
+    question: "A person starts from a point P walks 4 km towards east. He turns 90o left and walks 2km then he takes 90o left and walks 2km, then takes left turn and walks 4km reaches point R. Turns 90o right walks 2km. He turns right and walks 6Km reaches point S from there he walks 2km towards east and nally he takes right turn and walks 2km and reaches point T. In which direction p is from R",
+    answer: "North - west",
+    options: [
+      "South- east",
+      "East",
+      "North - west",
+      "South - West"
+    ]
+  },
+    {
+    numb: 7,
+    question: "A constable is 114m behind a thief .The constable runs 21m and the thief runs 15m in a minute. In what time will the constable catch the thief?",
+    answer: "19 min",
+    options: [
+      "18 min",
+      "17 min",
+      "16 min",
+      "19 min"
+    ]
+  },
+  
+
+    {
+    numb: 8,
+     question: "Choose the answer option that arranges the given set of words in the 'most' meaningful order. The words when put in order should make logical sense according to size, quality, occurrence of events, value, appearance, nature, process etc. 1. pebble 2. laptop 3. empty bottle 4. 1 kg sugar 5. paper",
+     answer: "1,5,3,4,2",
+     options: [
+      "3,4,2,1,5",
+      "3,4,1,2,5",
+     "4,1,3,2,5",
+       "1,5,3,4,2"
+     ]
+   },
+   {
+    numb:9 ,
+    question: "Find the next number in the series. 1,1,2,6,24,?",
+    answer: "48",
+    options: [
+      "54",
+      "102",
+      "210",
+      "120"
+    ]
+  },
+   {
+    numb: 10,
+     question: "I walk a certain distance and ride back taking a total time of 37 minutes. I could walk both ways in 55 minutes. How long would it take me to ride both ways?",
+     answer: "19 min",
+     options: [
+      "9.5 min",
+      "19 min",
+     "18 min",
+       "20 min"
+     ]
+   },
+   {
+    numb: 11,
+     question: "Excluding stoppages, the speed of the bus is 60 kmph and including stoppages, the speed is 45 kmph. For how many minutes does the bus stop per hour?",
+     answer: "15 min",
+     options: [
+      "20 min",
+      "40 min",
+     "15 min",
+       "25 min"
+     ]
+   },
+   {
+    numb: 12,
+    question: "Select the right option from the given alternatives.Introducing a man, a woman said, He is the husband of my mothers daughter. How is the woman related to the man",
+    answer: "None of these",
+    options: [
+      "Mother",
+      "Daughter",
+      "Sister",
+      "None of these"
+    ]
+  },
+   {
+    numb: 13,
+     question: "Two men starting from the same place walk at the rate of 8 kmph and 6kmph respectively. What time will they take to be 12 km a part, if they walk in the same direction?",
+     answer: "6hrs",
+     options: [
+      "8hrs",
+      "4hrs",
+     "6hrs",
+       "2hrs"
+     ]
+   },
+   {
+    numb: 14,
+     question: "A policeman sighted a robber from a distance of 600 m. The robber also noticed the policeman and started running at 16 km/hr. The policeman also started running after him at the speed of 20 km/hr. Find the distance that the robber would run before being caught?",
+     answer: "2.4 kmph",
+     options: [
+      "2.2 kmph",
+      "2 kmph",
+     "6hrs",
+       "3 kmph"
+     ]
+   },
+   {
+    numb: 15,
+    question: "Avinash lends Rs. 5000 to Ram and a certain sum to Umesh at same time at same rate of 5% p.a. simple interest. If after 4 years, Avinash altogether receives Rs. 2000 as interest from Ram and Umesh, then the sum lent to Umesh is:",
+    answer: "5000",
+    options: [
+      "5700",
+      "4000",
+      "6500",
+      "5000"
+    ]
+  },
+  {
+    numb: 16,
+    question: "If annual rate of simple interest increases from 15 % to 18 %, a man's yearly interest increases by 480. His principal (in rupees) is:",
+    answer: "16000",
+    options: [
+      "12000",
+      "14000",
+      "16000",
+      "15000"
+    ]
+  },
+  {
+    numb: 17,
+    question: "In a competition A, B, and C are participation. The probability that A wins is twice that of B. The probability that B wins is twice that of C. Then the probability that A losses is:",
+    answer: "3/7",
+    options: [
+      "7/5",
+      "3/7 kmph",
+      "6/5 kmph",
+      "4/8 kmph"
+    ]
+  },
+  {
+    numb: 18,
+    question: "After decreasing 24% in the price of an article costs Rs.912. Find the actual cost of an article?",
+    answer: "1200",
+    options: [
+      "1400",
+      "1300",
+      "1200",
+      "1100"
+    ]
+  },
+  {
+    numb:19 ,
+    question: "How many two-digit numbers leave the remainder 2 when divided by 5?",
+    answer: "18",
+    options: [
+      "18",
+      "28",
+      "50",
+      "59"
+    ]
+  },
+  {
+    numb:20 ,
+    question: "The diameter of two circles are the sides of a square and the diagonal of the square . The ratio of the area of the smaller circle to the larger circle is",
+    answer: "01 : 02",
+    options: [
+      "01:04",
+      "02 : 03",
+      "01 : 02",
+      "01 : 03"
+    ]
+  },
+  {
+    numb:21 ,
+    question: "A papaya was planted 2 years ago. It increases at the rate of 20% every year. If at present, the height of the tree is 540 cm, what was it when the tree was planted ?",
+    answer: "375 cms",
+    options: [
+      "324 cms",
+      "400 cms",
+      "432 cms",
+      "375 cms"
+    ]
+  },
+  {
+    numb: 22,
+    question: "If a, b, and c are positive real numbers such that a+b-c/c=a-b+c/b=-a+b+c/a, find the value of (a+b) (b+c) (c+a) /abc?",
+    answer: "8",
+    options: [
+      "9",
+      "4 ",
+      "7 ",
+      "8 "
+    ]
+  },
+  {
+    numb:23,
+    question: "If the length, breadth and height of a rectangular parallel piped are in the ratio 3 : 5 : 8 and total surface area is 7742 sq.m , then the length, breadth and height of parallel piped are ",
+    answer: "21,35 , 56",
+    options: [
+      "21,35 , 56",
+      "25, 40, 48",
+      "28, 52 , 24",
+      "None of the mentioned options"
+    ]
+  },
+  {
+    numb:24 ,
+    question: "Two pipes can fill a cistern in 4hours and 6hours respectively. Pipes are opened simultaneously and it is due to leakage in the bottom, 36min extra are taken for cistern to be filled up. If the cistern is full, in what time would the le",
+    answer: "12 hours",
+    options: [
+      "13 hours",
+      "12 hours",
+      "24 hours",
+      "6 hours"
+    ]
+  },
+  {
+    numb: 25,
+    question: "A combination lock on a suitcase has 3 wheels each labeled with 9 digits from 1 to 9. If an opening combination is a particular sequence of 3 digits with no repeats, what is the probability of a person guessing the right combination?",
+    answer: "1/504",
+    options: [
+      "17/504",
+      "1/504",
+      "5/504",
+      "7/504"
+    ]
+  },
+  {
+    numb: 26,
+    question: "If BCDI is coded as SLAP then LASP is coded as",
+    answer: "CDBI",
+    options: [
+      "IDBC",
+      "BDCI",
+      "CDBI",
+      "CBDT"
+    ]
+  },
+  {
+    numb: 27,
+    question: "Statements :Some apples are red Some fruits are apples Conclusions:(1)Some apples are green (2)  Fruit is Red",
+    answer: "Neither (1) nor (2) follows",
+    options: [
+      "Only (1) conclusion follows",
+      "Only (2) conclusion follows",
+      "Either (1) or (2) follows",
+      "Neither (1) nor (2) follows"
+    ]
+  },
+  {
+    numb:28 ,
+    question: "M, N, O, P and Q are five professionals working in same MNC. All of them works in different department, viz, IT, Admin, Marketing, Account and HR. They do these works on a particular day in a week from Monday to Friday. Q does not work on Tuesday and does not work in Admin. N works in Marketing but does not work on Monday or Friday. The person who works in Account works on Thursday. O does his work on Wednesday but he is not Admin. The person who works in IT works on Friday but not by P. M does his work on Monday. Which of the following tasks is done by O?",
+    answer: "HR",
+    options: [
+      "ACCOUNT",
+      "MARKETING",
+      "HR",
+      "Either (a) and (c)"
+    ]
+  },
+  {
+    numb: 29,
+    question: "Pointing to a photograph, a woman says,This man's son's sister is my mother-in-law. How is the woman's husband related to the man in the photograph? ",
+    answer: "Grand son",
+    options: [
+      "Nephew",
+      "Son",
+      "Grand son",
+      "Son-in-law"
+    ]
+  },
+  {
+    numb: 30,
+    question: "A + B means A is the mother of B. A - B means A is the sister of B. A * B means A is the father of B. A β B means A is the brother of B. Which of the following means that B is the maternal uncle of A?",
+    answer: "B β J - L + R – A",
+    options: [
+      "B - Y + G β A",
+      "A - Y * P – B",
+      "B β C + F * A",
+      "B β J - L + R – A"
+    ]
+  },
+  {
+    numb: 31,
+    question: "enamoured: He is enamoured of Physics and hence wants to be a scientist",
+    answer: "fascinated",
+    options: [
+      "love",
+      "abhor",
+      "fascinated",
+      "experimented"
+    ]
+  },
+  {
+    numb: 32,
+    question: "He had completed six years of service before his death.",
+    answer: "Six years of service had been completed by him before his death",
+    options: [
+      "Six years of service has been completed by him before his death",
+      "Six years of service have been completed by him before his death",
+      "Six years of service had been completed by him before his death",
+      "Six years of service was completed by him before his death"
+    ]
+  },
+  {
+    numb: 33,
+    question: "The soldiers received a military ",
+    answer: "mandate",
+    options: [
+      "allotment",
+      "affectation",
+      "dominion",
+      "mandate"
+    ]
+  },
+  {
+    numb: 7,
+    question: "humongous: He could not resit on accepting that humongous deal proposed for him.",
+    answer: "enormous",
+    options: [
+      "financial",
+      "enormous",
+      "interesting",
+      "strange"
+    ]
+  }
+];
